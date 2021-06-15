1d1fdff39145480bfe5c19f85298a936f4e59e5d
Andrés Fernández <andresavena19@gmail.com>
2021-06-15 12:00:46 -0500

    feat: generic events boilerplate

---
+++ src/mappings/index.ts
@@ -0,0 +1,20 @@
+import {
+	Approval,
+	ApprovalForAll,
+	Generated
+} from "../../generated/autoglyphs/autoglyphs";
+
+export * from "./transfer"
+
+export function handleApproval(event: Approval): void {
+
+}
+
+export function handleApprovalForAll(event: ApprovalForAll): void {
+
+}
+
+
+export function handleGenerated(event: Generated): void {
+
+}
